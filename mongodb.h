94f0986a64de959e8dd2d17b2c4e3ec3de80d7da
wzj1119 <52001757+wzj1119@users.noreply.github.com>
2019-10-21 17:46:03 +0800

    Add files via upload

---
+++ mongodb.h
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<sys/time.h>
+#include "libbson-1.0/bson.h"
+#include "libmongoc-1.0/mongoc.h"
+
+//void gettime();
+//void MongoDB(char *idfile,char*eventtype,char*url,char*mediatype,char*mediaformat);
+int MongoDB(char*arg);
