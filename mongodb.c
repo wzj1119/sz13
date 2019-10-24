a4a8aec52a89b1ec63a2e2811f824de5d7d78b4c
wzj1119 <52001757+wzj1119@users.noreply.github.com>
2019-10-24 17:13:11 +0800

    Add files via upload

--- mongodb.c
+++ mongodb.c
@@ -1,9 +1,8 @@
 #include "mongodb.h"
 
-
-/*char buf[24]; //存id_media
+char buf[24]; //存id_media
 char Saveid_key[36];  //存id_key
-void gettime()
+void gettime()   //获取key值和获取当前毫秒值
 {
     struct timeval tv;
     gettimeofday(&tv,NULL);
@@ -11,16 +10,16 @@ void gettime()
     long time=tv.tv_sec;  //当前时间（秒）
     memset(buf,0,24);
     sprintf(buf,"%ld",timeuse);
-    sprintf(Saveid_key,"%d%d",getpid(),time);
+    memset(Saveid_key,0,36);
+    sprintf(Saveid_key,"%d%d",getpid(),time);  //pid+当前时间（秒）生成key
 
-}*/
-//void MongoDB(char *idfile,char*eventtype,char*url,char*mediatype,char*mediaformat)
-int MongoDB(char*arg)
+}
+int MongoDB(char*idfile,char*simId,char*eventtype,char*url,char*mediatype,char*mediaformat)
 {
     mongoc_client_t *client;
-    mongoc_collection_t *mediaDBcollection;
-    mongoc_collection_t *tempDBcollection;
-    mongoc_cursor_t *cursor;
+    mongoc_collection_t *mediaDBcollection;  //media集合
+    mongoc_collection_t *tempDBcollection;   //temp集合
+    mongoc_cursor_t *cursor;  //游标
     bson_error_t error;
     const bson_t *doc;
     bson_t *query;
@@ -29,48 +28,58 @@ int MongoDB(char*arg)
     bson_t child;
     char *str;
     char *string;
-   // gettime();
+    gettime();
     mongoc_init(); //初始化libmongoc驱动
     bson_init(&opts);
     //创建连接对象
     client=mongoc_client_new("mongodb://cesiumei:123456@47.103.85.222:27017/?authSource=media_collection");
+
     //获取指定数据库和集合
     tempDBcollection=mongoc_client_get_collection(client,"media_collection","temp_collection");
     mediaDBcollection=mongoc_client_get_collection(client,"media_collection","media_collection");
     //query在这里添加一个键值对，查询的时候将只查询出匹配上的结果
     query=bson_new();
-    BSON_APPEND_UTF8(query,"id_file",arg);
+    BSON_APPEND_UTF8(query,"id_file",idfile); //根据id_file查询
+    BSON_APPEND_UTF8(query,"sim",simId); //根据sim卡号查询
     BSON_APPEND_DOCUMENT_BEGIN(&opts,"projection",&child);
-    BSON_APPEND_INT32(&child,"_id",0);
+    BSON_APPEND_INT32(&child,"_id",0);   //将_id,id_file,sim去除
+    BSON_APPEND_INT32(&child,"id_file",0);
+    BSON_APPEND_INT32(&child,"sim",0);
     bson_append_document_end(&opts,&child);
-    cursor=mongoc_collection_find_with_opts(tempDBcollection,query,&opts,NULL);
+    cursor=mongoc_collection_find_with_opts(tempDBcollection,query,&opts,NULL); //在temp中根据字段查询
 
     while(mongoc_cursor_next(cursor,&doc))
     {
+        if(!mongoc_collection_remove(tempDBcollection,MONGOC_REMOVE_NONE,doc,NULL,&error))  //将数据从temp集合中删除
+        {
+            fprintf(stderr,"Delete failed...%s\n",error.message);
+        }
         str=bson_as_json(doc,NULL);
-       // printf("str:%s\n",str);
-        bson=bson_new_from_json((const uint8_t *)str,-1,&error);
+       // printf("str:%s\n",str);   
+        bson=bson_new_from_json((const uint8_t *)str,-1,&error);  //json转bson文档
         if(!bson)
         {
                 fprintf(stderr,"%s\n",error.message);
-
+                return EXIT_FAILURE;
         }
         string=bson_as_json(bson,NULL);
-      /*  BSON_APPEND_UTF8(bson,"event_type",eventtype);
-        BSON_APPEND_UTF8(bson,"id_key",Saveid_key);
-        BSON_APPEND_UTF8(bson,"url",url);
-        BSON_APPEND_UTF8(bson,"media_type",mediatype);
-        BSON_APPEND_UTF8(bson,"media_format",mediaformat);
-        BSON_APPEND_UTF8(bson,"id_media",buf);*/
-        if (!mongoc_collection_insert (mediaDBcollection, MONGOC_INSERT_NONE, bson, NULL, &error))
-        {//插入文档
+        if(cursor!=NULL);
+        {
+            //将evtnt_type,id_key,url,media_type,media_format,id_media加入bson文档
+            BSON_APPEND_UTF8(bson,"event_type",eventtype);
+            BSON_APPEND_UTF8(bson,"id_key",Saveid_key);
+            BSON_APPEND_UTF8(bson,"url",url);
+            BSON_APPEND_UTF8(bson,"media_type",mediatype);
+            BSON_APPEND_UTF8(bson,"media_format",mediaformat);
+            BSON_APPEND_UTF8(bson,"id_media",buf);
+            if (!mongoc_collection_insert (mediaDBcollection, MONGOC_INSERT_NONE, bson, NULL, &error))  //将bson文档插入media集合中
+            {//插入文档
                 fprintf (stderr, "%s\n", error.message);
-        }
-        bson_destroy(bson);
-        bson_free(str);
-        bson_free(string);
-    }
+            }
 
+        }
+       bson_free(str);
+    }
     bson_destroy(query);
     mongoc_cursor_destroy(cursor);
     mongoc_collection_destroy(tempDBcollection);//释放表对象；
