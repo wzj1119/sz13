b57d2e86882dcf3a80fff875bc3fc537c8b7605f
wzj1119 <52001757+wzj1119@users.noreply.github.com>
2019-10-29 10:09:07 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@ typedef struct _stCallback{
      char          Savemediatype[6]; //存文件类型
      char          Savemediaformat[6];//文件类型
      char          Saveffmpeg[256];  //存ffmpeg要压缩的文件
+     char          Savecurl[256];
 }stCallback;
 
 //生成文件
@@ -46,10 +47,11 @@ int save15sFile( char *buffer,int len, char* h15sFileName)
 }
 
 //视频压缩
-int ffmpeg(int fileId,char*arg)
+int ffmpeg(char*str,int fileId,char*arg)
 {
     memset(arg,0,256);
-    sprintf(arg,"ffmpeg -i /home/wzj/worktest/15svedio/file/%d.mp4 -r 10 -b:v 320k -b:a 32k -y /home/wzj/worktest/15svedio/compressfileName/%d.mp4",fileId,fileId); //ffmppeg指令
+    sprintf(arg,"ffmpeg -i /home/wzj/worktest/15svedio/file/%s%d.mp4 -r 10 -b:v 320k -b:a 32k -y /home/wzj/worktest/15svedio/compressfileName/%s%d.mp4",str,fileId,str,fileId); //ffmppeg指令
+   //  sprintf(arg,"ffmpeg -i /root/15sMedia/file/%s%d.mp4 -r 10 -b:v 320k -b:a 32k -y /root/15sMedia/compressfileName/%s%d.mp4",str,fileId,str,fileId);
     FILE *fp=popen(arg,"r"); //建立管道
     if(!fp)
     {
@@ -58,6 +60,38 @@ int ffmpeg(int fileId,char*arg)
     pclose(fp);
     return 0;
 
+}
+//图像上传到http
+int curlpecture(char*sim,int fileId,char*arr)
+{
+    memset(arr,0,256);
+
+    sprintf(arr,"curl -H Expect: -F file=@/home/wzj/worktest/15svedio/file/%s%d.jpg http://139.159.195.55:8080/media/fu?f=%s%d.jpg",sim,fileId,sim,fileId);
+    // sprintf(arr,"curl -H Expect: -F file=@/root/15sMedia/file/%s%d.jpg http://139.159.195.55:8080/media/fu?f=%s%d.jpg",sim,fileId,sim,fileId);
+    FILE*fpl=popen(arr,"w");
+    if(!fpl)
+    {
+        return -1;
+    }
+    pclose(fpl);
+
+    return 0;
+}
+//视频上传到http
+int curlvedio(char*sim,int fileId,char*des)
+{
+    memset(des,0,256);
+
+    sprintf(des,"curl -H Expect: -F file=@/home/wzj/worktest/15svedio/compressfileName/%s%d.mp4 http://139.159.195.55:8080/media/fu?f=%s%d.mp4",sim,fileId,sim,fileId);
+    //sprintf(des,"curl -H Expect: -F file=@/root/15sMedia/compressfileName/%s%d.mp4 http://139.159.195.55:8080/media/fu?f=%s%d.mp4",sim,fileId,sim,fileId);
+    FILE*fpl=popen(des,"w");
+    if(!fpl)
+    {
+        return -1;
+    }
+    pclose(fpl);
+
+    return 0;
 }
 
 //读回调
@@ -68,7 +102,7 @@ void read_cb(struct bufferevent *bev, void*arg)
 
     // read data from cache
     memset(callback->cRecvBuff, 0, RECVSIZE);
-    recvlen = bufferevent_read(bev, callback->cRecvBuff, RECVSIZE); //接收数据 
+    recvlen = bufferevent_read(bev, callback->cRecvBuff, RECVSIZE); //接收数据
     if(recvlen > 0)
     {
         // copy data from cRecvBuff to m_RecvBuffer
@@ -83,6 +117,7 @@ void read_cb(struct bufferevent *bev, void*arg)
        int len=0;
        int num=0;
        int ffmnum=0;
+       int res=0;
        if(callback->m_nRecvedSize < 25)   //根据协议包头不足25，退出
        {
            break;
@@ -109,8 +144,7 @@ void read_cb(struct bufferevent *bev, void*arg)
 
        if(25+datalen>callback->m_nRecvedSize)
        {
-           qDebug("一包数据大小大于收到的数据大小...");
-            break;
+           break;
        }
 
        if(datalen>0)
@@ -123,6 +157,7 @@ void read_cb(struct bufferevent *bev, void*arg)
       {
           // printf("end of file, 发送结束符号到终端设备!\n");
            qDebug("end of file,发送结束标志到终端设备");
+           qDebug(callback->Savesim);
            struct evbuffer *sendbuf = evbuffer_new();
            byte msgHeader[2] = {'@', '@'};
            byte status = 0;
@@ -147,40 +182,45 @@ void read_cb(struct bufferevent *bev, void*arg)
            if(2==eventType) //开关门图像
            {
                 memset(callback->pName,0,SavePSize);
-                sprintf(callback->pName,"/home/wzj/worktest/15svedio/file/%d.jpg",Id);  //图片路径及名称
-                // sprintf(pName,"/root/15sMedia/file/%d.jpg",Id);
+                sprintf(callback->pName,"/home/wzj/worktest/15svedio/file/%s%d.jpg",callback->Savesim,Id);  //图片路径及名称
+                // sprintf(pName,"/root/15sMedia/file/%s%d.jpg",callback->Savesim,Id);
                 num=save15sFile(callback->Savedata,callback->m_RecvdataSize,callback->pName); //调用save15sFile
                 if(num!=0)
                 {
-                    qDebug("图像文件存入本地失败...");
+                    qDebug("图像存入本地失败...");
                 }
                 if(0==num)
                 {
-                    qDebug("图像文件存入本地成功...");
+                    qDebug("图像存入本地成功...");
                 }
                 memset(callback->pName,0,SavePSize);
                 sprintf(callback->pName,"%d",Id);  //文件ID转换为字符串
 
 
-                sprintf(callback->Saveurl,"http://139.159.195.55:8080/home/wzj/worktest/15svedio/file/%d.jpg",Id);  //url路径及图像文件名称
+                sprintf(callback->Saveurl,"http://139.159.195.55:8080/media/doc/%s%d.jpg",callback->Savesim,Id);  //url路径及图像文件名称
                 MongoDB(callback->pName,callback->Savesim,callback->Saveeventtype,callback->Saveurl,callback->Savemediatype,callback->Savemediaformat);  //将数据存入MongoDB
+                res=curlpecture(callback->Savesim,Id,callback->Savecurl);
+                if(res!=0)
+                {
+                   qDebug("图像上传http失败");
+                }
            }
             if(0==eventType || 1==eventType) //0：举报视频 1：紧急视频
             {
 
                 memset(callback->pName,0,SavePSize);
-                sprintf(callback->pName,"/home/wzj/worktest/15svedio/file/%d.mp4",Id);  //视频存储路径及名称
-             // sprintf(vName,"/root/15sMedia/file/%d.mp4",Id);
+                sprintf(callback->pName,"/home/wzj/worktest/15svedio/file/%s%d.mp4",callback->Savesim,Id);  //视频存储路径及名称
+             // sprintf(vName,"/root/15sMedia/file/%s%d.mp4",callback->Savesim,Id);
                 num=save15sFile(callback->Savedata,callback->m_RecvdataSize,callback->pName);//调用save15sFile
                 if(num!=0)
                 {
-                    qDebug("视频文件存入本地失败...");
+                    qDebug("视频存入本地失败...");
                 }
                 if(0==num)
                 {
-                    qDebug("视频文件存入本地成功...");
+                    qDebug("视频存入本地成功...");
                 }
-                ffmnum=ffmpeg(Id,callback->Saveffmpeg);
+                ffmnum=ffmpeg(callback->Savesim,Id,callback->Saveffmpeg);
                 if(ffmnum!=0)
                 {
                     qDebug("ffmpeg 压缩失败...");
@@ -192,8 +232,12 @@ void read_cb(struct bufferevent *bev, void*arg)
                 memset(callback->pName,0,SavePSize);
                 sprintf(callback->pName,"%d",Id);
 
-                sprintf(callback->Saveurl,"http://139.159.195.55:8080/home/wzj/worktest/15svedio/compressfileName/%d.mp4",Id); //url路径及视频文件名称
-                MongoDB(callback->pName,callback->Savesim,callback->Saveeventtype,callback->Saveurl,callback->Savemediatype,callback->Savemediaformat); //将数据存入MongoDB
+                sprintf(callback->Saveurl,"http://139.159.195.55:8080/media/doc/%s%d.mp4",callback->Savesim,Id); //url路径及视频文件名称
+                res=curlvedio(callback->Savesim,Id,callback->Savecurl);
+                if(res!=0)
+                {
+                    qDebug("视频上传http失败");
+                }
             }
              callback->m_RecvdataSize=0;  //每次将数据存成文件后数据体总和清零
       }
